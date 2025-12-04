1da0f6bfd1235c6dedb2844927d3958c674a4635
nafeenusair <quazinusair@gmail.com>
2025-12-04 19:02:21 +0600

    g

---
+++ Recursion/not_so_mobile.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int solve(bool &isBalanced) {
+    int Wl, Dl, Wr, Dr;
+    cin >> Wl >> Dl >> Wr >> Dr;
+
+    if (Wl == 0) {
+        Wl = solve(isBalanced);
+    }
+
+    if (Wr == 0) {
+        Wr = solve(isBalanced);
+    }
+
+    if (Wl * Dl != Wr * Dr) {
+        isBalanced = false;
+    }
+
+    return Wl + Wr;
+}
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        bool isBalanced = true;
+        solve(isBalanced);
+
+        if (isBalanced) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
+
+        if (T > 0) {
+            cout << endl;
+        }
+    }
+
+    return 0;
+}
---
+++ Recursion/popes.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int Y, P, max_popes, best_first, best_last;
+vector<int> years;
+
+void solve_recursive(int left, int right) {
+    if (right == P) return;
+
+    if (years[right] >= years[left] + Y) {
+        solve_recursive(left + 1, right);
+    } else {
+        int current = right - left + 1;
+        if (current > max_popes) {
+            max_popes = current;
+            best_first = years[left];
+            best_last = years[right];
+        }
+        solve_recursive(left, right + 1);
+    }
+}
+
+int main() {
+    while (cin >> Y) {
+        cin >> P;
+        years.resize(P);
+        for (int i = 0; i < P; ++i) {
+            cin >> years[i];
+        }
+
+        max_popes = 0;
+        best_first = 0;
+        best_last = 0;
+
+        solve_recursive(0, 0);
+
+        cout << max_popes << " " << best_first << " " << best_last << "\n";
+    }
+    return 0;
+}
